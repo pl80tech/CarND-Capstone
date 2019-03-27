25d54713de8aa8ef8c675b3504b27613859135d4
pl80tech <pl80.tech@gmail.com>
2019-03-27 20:45:08 +0900

    Update loop function for DBW node

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -72,6 +72,14 @@ class DBWNode(object):
             #                                                     <any other argument you need>)
             # if <dbw is enabled>:
             #   self.publish(throttle, brake, steer)
+            if not None in (self.current_vel, self.linear_vel, self.angular_vel):
+                self.throttle, self.brake, self.steering = self.controller.control(self.current_vel,
+                                                                    self.dbw_enabled,
+                                                                    self.linear_vel,
+                                                                    self.angular_vel)
+
+            if self.dbw_enabled:
+                self.publish(self.throttle, self.brake, self.steering)
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
