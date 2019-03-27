f6d5aa61f466408b6a81d4413d4ea7077aa35d90
pl80tech <pl80.tech@gmail.com>
2019-03-27 20:35:30 +0900

    Correct the function name

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -54,7 +54,7 @@ class WaypointUpdater(object):
                 self.publish_waypoints(closet_waypoint_idx)
             rate.sleep()
 
-    def get_closest_waypoint_id(self):
+    def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
