3e474aa4997ed9758284cb5b6e157a88c28c4947
pl80tech <pl80.tech@gmail.com>
2019-03-27 20:07:23 +0900

    Implement get_closest_waypoint_id function

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -50,6 +50,27 @@ class WaypointUpdater(object):
                 self.publish_waypoints(closet_waypoint_idx)
             rate.sleep()
 
+    def get_closest_waypoint_id(self):
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+
+        # Check if closest is ahead or behind vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx-1]
+
+        # Equation for hyperplane through closest_coords
+        cl_vect = np.array(closest_coord)
+        prev_vect  = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+
+        val = np.dot(cl_vect-prev_vect, pos_vect-cl_vect)
+
+        if (val > 0):
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)  
+        return closest_idx
+
+
     def pose_cb(self, msg):
         # TODO: Implement
         self.pose = msg
