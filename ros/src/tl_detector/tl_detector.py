b4cbb83f2b0a2fc4e179d6a02a350b3da7ae1d25
pl80tech <pl80.tech@gmail.com>
2019-03-29 16:00:16 +0900

    Temporarily use the value from simulator for testing

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -120,6 +120,9 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
+        # For testing, just return the light state
+        return light.state
+        """
         if(not self.has_image):
             self.prev_light_loc = None
             return False
@@ -128,6 +131,7 @@ class TLDetector(object):
 
         #Get classification
         return self.light_classifier.get_classification(cv_image)
+        """
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
