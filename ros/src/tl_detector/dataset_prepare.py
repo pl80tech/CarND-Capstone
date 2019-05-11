dabb207f6455fe9f2dc2457d2085b66ee0d3e193
pl80tech <pl80.tech@gmail.com>
2019-05-11 12:49:58 +0900

    Add gid to download dataset#1 and remove redundant code

--- ros/src/tl_detector/dataset_prepare.py
+++ ros/src/tl_detector/dataset_prepare.py
@@ -19,33 +19,8 @@ def main():
     
     # Get id and filename
     if dataset == 1:
-        # A small subset of sample data (30 images) for quick debug
-        gid = '1Vik42Q_qMV_0tlXBZK9WxvYM3tToxQU2'
-        filename = "dataset1.zip"
-    elif dataset == 2:
-        # Sample data (from lecture)
-        gid = '1CWP4K-HlOqaOUXTMAQ8_vPP5FzP1pYwH'
-        filename = "dataset2.zip"
-    elif dataset == 3:
-        # Simulated data (from simulator)
-        # 2.5 round counter clockwise
-        gid = '1cndsQ6f-0ndbBEWUgZdX0sSomv1PZPap'
-        filename = "dataset3.zip"
-    elif dataset == 4:
-        # Simulated data (from simulator)
-        # 3 round clockwise
-        gid = '1akOvfxOe8Ln8BhxBDGzFX2XxMhqLtdia'
-        filename = "dataset4.zip"
-    elif dataset == 5:
-        # Simulated data (from simulator)
-        # Additional data to improve the behavior on quick & difficult curve
-        gid = '1Agmy5leXNWjQTVCnmfsGnOWrZtr-UxWh'
-        filename = "dataset5.zip"
-    elif dataset == 6:
-        # Simulated data (from simulator)
-        # Additional data to improve the drive on specific place (keep center)
-        gid = '1ir9Gyi0pglAeYynQIBt138mgNIsKcukn'
-        filename = "dataset6.zip"
+        gid = '1NJ8mDGhX16NksFNfpKvPlEt4UVIqhIL3'
+        filename = "dataset.zip"
     else:
         print("Not available yet. To be added")
 
