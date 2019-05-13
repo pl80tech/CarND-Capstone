157015d9185896a26ccfa5aa66673e4bbdfd3af7
pl80tech <pl80.tech@gmail.com>
2019-05-14 07:15:04 +0900

    Load retrained model for classifying traffic light

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,14 @@
 from styx_msgs.msg import TrafficLight
+import traffic_light_detection as tl_detection
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        self.path_to_graph = 'model/final/frozen_inference_graph.pb'
+        self.path_to_label = 'model/final/tl_detect_label_map.pbtxt'
+        self.detection_graph = None
+        self.category_index = None
+        self.detection_graph, self.category_index = tl_detection.get_final_model_info(self.path_to_graph, self.path_to_label)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
