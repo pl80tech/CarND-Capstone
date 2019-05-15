77080cdd01f9a65e73241ceeb966ca263f9cae32
pl80tech <pl80.tech@gmail.com>
2019-05-15 18:13:36 +0900

    Comment out strict version check to run on tensorflow v1.3

--- ros/src/tl_detector/traffic_light_detection.py
+++ ros/src/tl_detector/traffic_light_detection.py
@@ -18,8 +18,8 @@ from PIL import Image
 sys.path.append("..")
 from utils import ops as utils_ops
 
-if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
-  raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
+#if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
+#  raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
 
 from utils import label_map_util
 
