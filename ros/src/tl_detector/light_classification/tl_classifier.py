b541a2e277abe658ff80a4904c22616248a12693
pl80tech <pl80.tech@gmail.com>
2019-06-02 21:25:52 +0900

    Minor change

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -20,10 +20,6 @@ class TLClassifier(object):
             self.path_to_graph = config['path_to_graph']
             self.path_to_label = config['path_to_label']
             self.detection_threshold = config['detection_threshold']
-        
-        config_file.close()
-        
-        if (self.use_pretrained_tf_model):
             self.detection_graph = None
             self.category_index = None
             self.detection_graph, self.category_index = tl_detection.get_final_model_info(self.path_to_graph, self.path_to_label)
@@ -44,6 +40,9 @@ class TLClassifier(object):
         else:
             # Use newly built model
             rospy.loginfo("Use newly built model for traffic light classification")
+
+        # Close the config file
+        config_file.close()
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -79,6 +78,6 @@ class TLClassifier(object):
                 rospy.loginfo("No traffic light is detected. Keep the last light state")
         else:
             # Use newly built model
-            rospy.loginfo("Start topredict traffic light")
+            rospy.loginfo("Start top redict traffic light")
 
         return self.detected_light
