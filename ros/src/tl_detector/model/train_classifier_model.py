da6e96c5469dda976863c8f8d2b573557b1a81ce
pl80tech <pl80.tech@gmail.com>
2019-06-09 07:52:37 +0900

    Add NVIDIA models (customized from my previous projects)

--- ros/src/tl_detector/model/train_classifier_model.py
+++ ros/src/tl_detector/model/train_classifier_model.py
@@ -1,6 +1,54 @@
-# Main function
 import sys
+from keras.models import Sequential
+from keras.layers import Lambda, Cropping2D, Conv2D, Flatten, Dense, Dropout
 
+# Add layers for NVIDIA model (Architecture #1)
+def Nvidia_model_1(model, useDropout):
+    model.add(Conv2D(24, (5,5), strides=(2,2), activation='relu'))
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Conv2D(36, (5,5), strides=(2,2), activation='relu'))
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Conv2D(48, (5,5), strides=(2,2), activation='relu'))
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(4))
+
+    return model
+
+# Add layers for NVIDIA model (Architecture #2)
+def Nvidia_model_2(model, useDropout):
+    model.add(Conv2D(24, (5,5), strides=(2,2), activation='relu'))
+    model.add(Conv2D(36, (5,5), strides=(2,2), activation='relu'))
+    model.add(Conv2D(48, (5,5), strides=(2,2), activation='relu'))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(Flatten())
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Dense(100))
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Dense(50))
+    if useDropout == "True":
+        model.add(Dropout(0.2))
+    model.add(Dense(10))
+    model.add(Dense(4))
+
+    return model
+
+# Main function
 def main():       
     # Initial setting and hyperparameters
     modelArch = sys.argv[1]
