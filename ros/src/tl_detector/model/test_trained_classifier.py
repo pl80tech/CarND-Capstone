e9747ef6a4a55d40212feb99a99f1ce5c677e09c
pl80tech <pl80.tech@gmail.com>
2019-06-10 06:43:24 +0900

    Function to predict all images in specified folder and output images with classification result

--- ros/src/tl_detector/model/test_trained_classifier.py
+++ ros/src/tl_detector/model/test_trained_classifier.py
@@ -1,11 +1,42 @@
 import os
 import sys
+import numpy as np
+import cv2
 import tensorflow as tf
 from keras import backend as K
 from keras.models import load_model
 
 # Run prediction for all images in specified folder and output images with classification result
 def runPrediction(model, input_path, output_path):
+	images = os.listdir(input_path)
+	images.sort()
+	for image in images:
+		imagepath = input_path + '/' + image
+		img = cv2.imread(imagepath)
+		img_resize = cv2.resize(img, (320, 240))
+		img_reshape = np.reshape(img_resize, (1, 240, 320, 3))
+		score_list = model.predict(img_reshape)
+		light_type = np.argmax(score_list)
+		#print("light_type = " + str(light_type))
+
+		# Put text to image
+		if light_type == 0:
+			text = 'Traffic light: RED'
+			color = (0,0,255)
+		elif light_type == 1:
+			text = 'Traffic light: YELLOW'
+			color = (0,255,255)
+		elif light_type == 2:
+			text = 'Traffic light: GREEN'
+			color = (0,255,0)
+		else:
+			text = 'Traffic light: UNKNOWN'
+			color = (255,0,0)
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		cv2.putText(img, text, (50, 50), font, 2, color, thickness=2)
+
+		# Save image
+		cv2.imwrite(output_path + '/' + image, img)
 
 # Main function
 def main():
